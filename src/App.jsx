7420518406bc055d4a3ae72682677c4e55d28275
Yahaya Abdulbari <yahayaabdulbari13@gmail.com>
2026-09-21 12:12:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ import KycReceipts from './pages/admin/KycReceipts';
 // maintenance page
 import Maintenance from "./pages/Maintenance";
 // set maintenance
-const maintenanceMode = false;
+const maintenanceMode = true;
 
 // Protected Route Components
 const ProtectedRoute = ({ children, isAllowed, redirectTo }) => {
